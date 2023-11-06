0ba724536c13d171d7230235c98608f40b5b4a8e
GizIOn <34921969+IlgizMargamov@users.noreply.github.com>
2023-11-07 02:55:40 +0500

    что-то не так

--- web/src/jsMain/kotlin/main.kt
+++ web/src/jsMain/kotlin/main.kt
@@ -1,10 +1,20 @@
-
 import kotlinx.browser.document
-import kotlinx.html.js.onClickFunction
-import react.*
-import react.dom.button
+import kotlinx.browser.window
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.html.InputType
+import kotlinx.html.js.onChangeFunction
+import org.khronos.webgl.Int8Array
+import org.w3c.dom.HTMLInputElement
+import org.w3c.files.FileList
+import org.w3c.files.FileReaderSync
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
 import react.dom.div
-import react.dom.p
+import react.dom.img
+import react.dom.input
 import react.dom.render
 
 fun main(): Unit = run {
@@ -16,35 +26,69 @@ fun main(): Unit = run {
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 class App : RComponent<RProps, RState>() {
-    override fun RBuilder.render() {
-        div {
-            child(functionalComponent<RProps> {
-                val (count, setCount) = useState(0)
-                button {
-                    attrs.onClickFunction = {setCount(count+1)}
-                    +"press me!"
-                    +"$count"
-                }
-            })
-            child(StringList::class) {
-                attrs.lines = listOf("a", "b", "c")
+
+    private val scope = MainScope()
+    private var photos = emptyList<String>()
+
+    private suspend fun processImage(imageData: ByteArray) {
+        ApiClient.postPhotoToRootAlbum(imageData)
+        reloadPage()
+    }
+
+    private fun handleImageUpload(fileList: FileList?) {
+        val file = fileList?.item(0)
+        if (file != null) {
+            scope.launch {
+                val reader = FileReaderSync()
+                val arrayBuffer = reader.readAsArrayBuffer(file).run { Int8Array(this) as ByteArray }
+                processImage(arrayBuffer)
             }
         }
     }
-}
 
-external interface StringListProps : RProps {
-    var lines: List<String>
-}
+    private fun reloadPage() {
+        window.location.reload()
+    }
 
-@OptIn(ExperimentalJsExport::class)
-@JsExport
-class StringList : RComponent<StringListProps, RState>() {
+    private suspend fun loadPhotos() {
+        val photoIds = ApiClient.getRootAlbumPhotoIds()
+        val loadedPhotos = mutableListOf<String>()
+
+        for (photoId in photoIds) {
+            val photoData = ApiClient.getPhotoById(photoId)
+            loadedPhotos.add(photoData)
+        }
+
+        photos = loadedPhotos
+    }
+
     override fun RBuilder.render() {
-        for (line in props.lines) {
-            p {
-                +line
+        div {
+            input(type = InputType.file) {
+                attrs {
+                    onChangeFunction = { event ->
+                        val fileList = (event.target as HTMLInputElement).files
+                        handleImageUpload(fileList)
+                    }
+                    accept = "image/*"
+                }
             }
+
+            div {
+                for (photoData in photos) {
+                    img {
+                        attrs {
+                            src = "data:image/jpeg;base64,${photoData}"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    init {
+        scope.launch {
+            loadPhotos()
         }
     }
 }
